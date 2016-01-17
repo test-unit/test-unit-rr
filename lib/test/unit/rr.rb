5caaf3557069b75491c293563d9a25b03ecc732c
Kouhei Sutou <kou@clear-code.com>
2016-01-17 23:34:53 +0900

    Add workaround to unregister auto RR adaptation
    It should be done in RR.

--- lib/test/unit/rr.rb
+++ lib/test/unit/rr.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2011-2013  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2011-2016  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -14,6 +14,9 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+# TODO: It should be done in RR.
+require "rr/without_autohook"
+RR.adapters.clear
 require "rr"
 require "test-unit"
 require "test/unit/rr/version"
