cda767132c5cf9e8c8dc37e560b99445712c06a8
Kouhei Sutou <kou@clear-code.com>
2016-01-17 23:37:45 +0900

    Bump version

--- lib/test/unit/rr/version.rb
+++ lib/test/unit/rr/version.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012-2013  Kouhei Sutou <kou@clear-code.com>
+# Copyright (C) 2012-2016  Kouhei Sutou <kou@clear-code.com>
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -17,7 +17,7 @@
 module Test
   module Unit
     module RR
-      VERSION = "1.0.4"
+      VERSION = "1.0.5"
     end
   end
 end
